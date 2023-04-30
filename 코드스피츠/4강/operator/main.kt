d5903dce1dd7a65e0309ecf5fc394f8c9e95c39e
g <ruddmsdl000@naver.com>
2023-05-01 00:21:03 +0900

    Create main.kt

---
+++ 코드스피츠/4강/operator/main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    class Op (var value: Int){
+        operator fun plus(add:Int):Int{
+            return value + add
+        }
+    }
+
+    println(Op(5) + 3 == 8)
+    println(Op(5).plus(3) == 8)
+}
